e33f4ea10b03c2d9503bf57da87d1fbddafda62b
Banana_Cat1337 <7862403@gmail.com>
2024-01-22 12:03:53 +0700

    five2

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -3,33 +3,32 @@ import readlineSync from 'readline-sync';
 import { name } from './brain-games.js';
 
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-
-export const brainEven = () => {
-    let correct = 0
-    while (correct < 3) {
-        const number = Math.floor(Math.random() * 10);
-        const isEven = number % 2 === 0;
-        console.log('Question: ' + number);
-        const str = readlineSync.question('Your answer: ');
-        if (isEven && str === 'yes') {
-            console.log('Correct!')
-            correct += 1;
-        } else if (isEven && str !== 'yes') {
-            console.log(`'${str}' is wrong answer ;(. Correct answer was 'yes'.
-        Let's try again, ${name}!`)
-            correct = 0;
-        } else if (!isEven && str === 'no') {
-            console.log('Correct!')
-            correct += 1;
-        } else if (!isEven && str !== 'no') {
-            console.log(`'${str}' is wrong answer ;(. Correct answer was 'no'.
-        Let's try again, ${name}!`)
-            correct = 0;
-        }
+const brainEven = () => {
+  let correct = 0;
+  while (correct < 3) {
+    const number = Math.floor(Math.random() * 10);
+    const isEven = number % 2 === 0;
+    console.log(`Question: ${number}`);
+    const str = readlineSync.question('Your answer: ');
+    if (isEven && str === 'yes') {
+      console.log('Correct!');
+      correct += 1;
+    } else if (isEven && str !== 'yes') {
+      console.log(`'${str}' is wrong answer ;(. Correct answer was 'yes'.
+        Let's try again, ${name}!`);
+      correct = 0;
+    } else if (!isEven && str === 'no') {
+      console.log('Correct!');
+      correct += 1;
+    } else if (!isEven && str !== 'no') {
+      console.log(`'${str}' is wrong answer ;(. Correct answer was 'no'.
+        Let's try again, ${name}!`);
+      correct = 0;
     }
-    if (correct === 3) {
-    console.log(`Congratulations, ${name}!`)
-}
-}
-brainEven()
+  }
+  if (correct === 3) {
+    console.log(`Congratulations, ${name}!`);
+  }
+};
+brainEven();
+export default brainEven;
