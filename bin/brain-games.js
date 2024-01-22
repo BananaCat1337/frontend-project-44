dc0d9c4f237c96dda9c268549de75c12f0bf5fc5
Banana_Cat1337 <7862403@gmail.com>
2024-01-22 11:57:00 +0700

    five

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-
 console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('Your answer: ');
+export const name = readlineSync.question('Your answer: ');
 console.log(`Hello, ${name}!`);
