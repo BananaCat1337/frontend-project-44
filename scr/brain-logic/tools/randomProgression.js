4a530dc279e49782af0ec28f92381725b9f90453
Banana_Cat1337 <7862403@gmail.com>
2024-03-01 03:01:01 +0700

    gimnpapicha2.3

--- scr/brain-logic/tools/randomProgression.js
+++ scr/brain-logic/tools/randomProgression.js
@@ -3,7 +3,7 @@ const generateRandomNumber = (min, max) => Math.floor(Math.random() * (max - min
 const randomProgression = () => {
   const start = generateRandomNumber(1, 10);
   const step = generateRandomNumber(1, 5);
-  const length = generateRandomNumber(5, 10);
+  const length = generateRandomNumber(6, 10);
   const hiddenIndex = generateRandomNumber(0, length - 1);
   const progression = [];
   for (let i = 0; i < length; i += 1) {
