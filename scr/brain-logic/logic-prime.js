b8d532a7dcc856a0a0b01554e08dc7f0b0c90359
Banana_Cat1337 <7862403@gmail.com>
2024-02-29 12:19:28 +0700

    lastFinish2

--- scr/brain-logic/logic-prime.js
+++ scr/brain-logic/logic-prime.js
@@ -1,6 +1,7 @@
 import playRounds from './tools/rounds.js';
 
 const logicPrime = () => {
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
   playRounds('prime');
 };
 
