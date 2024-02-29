88752f2071a644438671bf44ca3a79f9362fdb62
Banana_Cat1337 <7862403@gmail.com>
2024-03-01 02:52:17 +0700

    gimnpapicha2.2

--- scr/brain-logic/tools/RandomOperator.js
+++ scr/brain-logic/tools/RandomOperator.js
@@ -3,8 +3,8 @@ import generateRandomNumber from './randomNumberGeneration.js';
 const randomExpression = () => {
   const operators = ['+', '-', '*'];
   const operator = operators[Math.floor(generateRandomNumber(operators.length))];
-  const num1 = generateRandomNumber(10);
-  const num2 = generateRandomNumber(10);
+  const num1 = generateRandomNumber(5);
+  const num2 = generateRandomNumber(5);
   return `${num1} ${operator} ${num2}`;
 };
 
